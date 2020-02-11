28659b7231909ba84ef625c281bbd6237a9a5c6e
Marcelo Puebla <mpuebla@focasoftware.com>
2020-02-11 17:50:36 -0300

    Add Template inicial del objeto a devolver

--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,15 @@
 var router = require('express').Router();
 var fetch = require("node-fetch");
+var transform = require('json-to-json-transformer').transform;
+var templates = require('../templateObjects/templates');
 
 router.get('', (req, res, next) => {
   const query = req.query.q;
   fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}&limit=4`)
     .then(res => res.json())
     .then(function (data) {
-      res.status(200).send(data);
+      var newObj = transform(templates.templateItems, data);
+      res.status(200).send(newObj);
     });
 });
 
---
+++ templateObjects/templates.js
@@ -0,0 +1,14 @@
+module.exports.templateItems = {
+  author: {
+    name: 'Marcelo',
+    lastname: 'Puebla'
+  },
+  items: '{{results}}',
+};
+
+module.exports.templateItem = {
+  author: {
+    name: 'Marcelo',
+    lastname: 'Puebla'
+  },
+};
