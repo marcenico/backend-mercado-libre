2b210b0a4688fe3df3cff792abc9ab91d7990946
mpuebla <mpuebla@focasoftware.com>
2020-02-15 17:09:14 -0300

    Change Cambio imagen que se envia

--- utils/templates.js
+++ utils/templates.js
@@ -55,7 +55,7 @@ module.exports.templateItem = {
       amount: '{{available_quantity}}',
       decimals: '{{price}}'
     },
-    picture: '{{thumbnail}}',
+    picture: '{{pictures[0].url}}',
     condition: '{{condition}}',
     free_shipping: '{{shipping.free_shipping}}',
     sold_quantity: '{{sold_quantity}}',
